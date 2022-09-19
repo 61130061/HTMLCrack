9831f6a827e0641a22689a4243bf24a2a0f16a89
61130061 <thezeroyou@gmail.com>
2022-09-19 21:54:04 +0700

    ➕ create react app with tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-800 h-screen text-white p-10">
+       <div className="text-3xl font-bold">Hello HTML Crack</div>
+    </div>
+  );
+}
+
+export default App;
